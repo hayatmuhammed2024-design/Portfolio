9b698ec4579a6e9ca2a15284fcbff93323d30798
hayat muhammed seid <hayatmuhammed2024@gmail.com>
2026-05-05 16:41:09 +0300

    final professional portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+// Scroll animation
+const elements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+elements.forEach(el => observer.observe(el));
